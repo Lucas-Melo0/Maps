c5b70deea3b9dfd15c286a09535ee6b7b257c088
Lucas-Melo0 <lucassss8v@gmail.com>
2022-11-01 08:16:35 -0300

    feat: Iterating over a map

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#ffff00",
+		"white": "#FFFFFF",
 	}
-	fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
